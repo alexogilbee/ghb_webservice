8cf8362fab44f9492ff33120169049795585e8e5
alexogilbee <alexogilbee@github.com>
2021-10-12 18:45:16 -0400

    should work now? hopefully

--- ghb_webservice/__main__.py
+++ ghb_webservice/__main__.py
@@ -13,7 +13,7 @@ from pprint import pprint
 
 print("Hey Howdy Holmes")
 
-client = MongoClient("mongodb+srv://user:12345@test-server.scule.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+client = MongoClient("mongodb+srv://user:12345@test-server.scule.mongodb.net/test-server?retryWrites=true&w=majority")
 db = client.issueBot
 serverStatusResult=db.command("serverStatus")
 pprint(serverStatusResult)
